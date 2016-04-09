514b5bf3f6ba7b60e8a2a94e6e6316af277eb615
Pierre Rudloff <contact@rudloff.pro>
2016-04-09 20:10:03 +0200

    Get infobox from main article

--- NewsTab.php
+++ NewsTab.php
@@ -10,4 +10,23 @@ class NewsTab
             $links['namespaces']['adresse_talk']['text'] = 'Actualités';
         }
     }
+
+    public static function getInfobox(&$article)
+    {
+        global $wgOut;
+        $curTitle = $article->getTitle();
+        $mainTitle = $curTitle->getSubjectPage();
+        if ($curTitle->getNamespace() == NS_ADDRESS_NEWS) {
+            $mainArticle = \Article::newFromTitle($mainTitle, $article->getContext());
+            $mainContent = $mainArticle->getPage()->getContent();
+            if (isset($mainContent)) {
+                $header = $mainContent->getSection(0)->serialize();
+                preg_match('/{{Infobox adresse(.*)}}/si', $header, $matches);
+                $header = '{{Infobox adresse'.$matches[1].
+                    '|lien = '.$mainTitle->getCanonicalURL().
+                    '}}';
+                $wgOut->addWikiText($header);
+            }
+        }
+    }
 }
