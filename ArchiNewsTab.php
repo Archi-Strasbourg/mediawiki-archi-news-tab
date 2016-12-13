bba7ec06262c7a3972786188395b9a2e07d68fe8
Pierre Rudloff <contact@rudloff.pro>
2016-12-13 19:06:58 +0100

    Docblocks

--- ArchiNewsTab.php
+++ ArchiNewsTab.php
@@ -1,11 +1,21 @@
 <?php
-
+/**
+ * ArchiNewsTab class
+ */
 namespace ArchiNewsTab;
 
 use SectionsCount\SectionsCount;
 
+/**
+ * Add a custom news tab on top of every address page
+ */
 class ArchiNewsTab
 {
+    /**
+     * Get the title of the new tab
+     * @param  Title  $title Title of the current article
+     * @return string
+     */
     private static function getNewsTabTitle(\Title $title)
     {
         global $wgParser;
@@ -21,7 +31,13 @@ class ArchiNewsTab
         }
     }
 
-    public static function replaceTabs($skin, &$links)
+    /**
+     * Replace tabs with custom tab
+     * @param  \Skin $skin  Current skin
+     * @param  array $links Tab links
+     * @return void
+     */
+    public static function replaceTabs(\Skin $skin, &$links)
     {
         $curTitle = $skin->getTitle();
         $namespace = $curTitle->getNamespace();
@@ -59,7 +75,12 @@ class ArchiNewsTab
         }
     }
 
-    public static function getInfobox(&$article)
+    /**
+     * Extract and output the infobox from an address article
+     * @param  \Article $article Article to extract the infobox from
+     * @return void
+     */
+    public static function getInfobox(\Article &$article)
     {
         global $wgOut;
         $curTitle = $article->getTitle();
@@ -75,7 +96,13 @@ class ArchiNewsTab
         }
     }
 
-    public static function talkpagename($parser, $title = null)
+    /**
+     * Customize the link to talk pages
+     * @param  \Parser $parser MediaWiki parser
+     * @param  string $title  Current page title
+     * @return string
+     */
+    public static function talkpagename(\Parser $parser, $title = null)
     {
         $t = Title::newFromText($title);
         $newTitle = \Title::newFromText($t->getText(), NS_ADDRESS_NEWS);
@@ -83,13 +110,26 @@ class ArchiNewsTab
         return wfEscapeWikiText($newTitle->getPrefixedText());
     }
 
+    /**
+     * Register new magic words
+     * @param  array $variableIds Existing magic word IDs
+     * @return void
+     */
     public static function registerMagicWord(&$variableIds)
     {
         $variableIds[] = 'newspagename';
         $variableIds[] = 'newsparentpagename';
     }
 
-    public static function getMagicWord(&$parser, &$cache, &$magicWordId, &$ret)
+    /**
+     * Parse a magic word and return the result
+     * @param  \Parser $parser     MediaWiki parser
+     * @param  array $cache
+     * @param  string $magicWordId Magic word ID (newspagename or newsparentpagename)
+     * @param  string $ret         Returned text
+     * @return bool Always true
+     */
+    public static function getMagicWord(\Parser &$parser, &$cache, &$magicWordId, &$ret)
     {
         if ($magicWordId == 'newspagename') {
             $t = $parser->getTitle();
